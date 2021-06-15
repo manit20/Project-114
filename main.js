b9c3af7819f66e3180a841a695e183b909063b21
manit20 <78215866+manit20@users.noreply.github.com>
2021-06-15 19:36:14 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+function preload(){}
+
+function setup(){
+    canvas = createCanvas(320, 300);
+    canvas.center();
+}
+
+function draw(){}
+
+function take_snapshot(){
+    save("filter_image.png");
+}
